2fdefae99a4e8e5e7b7787cee57f23f64bbe41d1
Quang <qmle@uark.edu>
2021-06-17 10:22:37 -0500

    commit to merge with dev on pycharm (windows), since vscode does not have correct PYHONPATH

--- CmdRun/cmd.py
+++ CmdRun/cmd.py
@@ -2,10 +2,10 @@
 import sys, os
 #sys.path.append('..')
 # Set relative location
-#cur_path =sys.path[0] # get current path (meaning this file location)
-#cur_path = cur_path[0:-11] #exclude "powercad/cmd_run"
-#print(cur_path)
-#sys.path.append(cur_path)
+cur_path =sys.path[0] # get current path (meaning this file location)
+cur_path = cur_path[0:-11] #exclude "powercad/cmd_run"
+print(cur_path)
+sys.path.append(cur_path)
 
 from core.model.electrical.electrical_mdl.cornerstitch_API import CornerStitch_Emodel_API, ElectricalMeasure 
 from core.model.thermal.cornerstitch_API import ThermalMeasure
