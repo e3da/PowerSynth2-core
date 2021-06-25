16f798ebbabbf1381b07dd73dd54962778a0e797
Joshua Mitchener <joshuamitchenerlotr@gmail.com>
2021-06-25 17:06:00 -0500

    Improved Introductory Window

--- NEW_UI/macro.py
+++ NEW_UI/macro.py
@@ -11,9 +11,19 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(328, 105)
+        Dialog.resize(388, 246)
         self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
         self.verticalLayout.setObjectName("verticalLayout")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setText("")
+        self.label_2.setPixmap(QtGui.QPixmap("./NEW_UI/ui/compressed.png"))
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout.addWidget(self.label_2)
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_3.setObjectName("label_3")
+        self.verticalLayout.addWidget(self.label_3)
         self.label = QtWidgets.QLabel(Dialog)
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
@@ -34,10 +44,10 @@ class Ui_Dialog(object):
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.label.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:11pt; font-weight:600;\">Run Macro or Create Macro?</span></p></body></html>"))
+        self.label_3.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:14pt; font-weight:600; font-style:italic;\">Welcome to PowerSynth 2.0!</span></p></body></html>"))
+        self.label.setText(_translate("Dialog", "<html><head/><body><p><span style=\" font-size:10pt;\">Would you like to run a pre-existing macro or create a new one?</span></p></body></html>"))
         self.pushButton.setText(_translate("Dialog", "Run Macro"))
         self.pushButton_2.setText(_translate("Dialog", "Create Macro"))
-
 
 if __name__ == "__main__":
     import sys
