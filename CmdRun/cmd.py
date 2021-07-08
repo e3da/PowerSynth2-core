b2540cf1339b0bd591396d1ee2aec7f72b209315
Imam <ialrazi@uark.edu>
2021-07-08 16:15:22 -0500

    evaluated journal case 2. Facing issues with case 1

--- CmdRun/cmd.py
+++ CmdRun/cmd.py
@@ -1068,7 +1068,8 @@ if __name__ == "__main__":
                    {qmle_nethome:'Unit_Test_Cases/Case_0_0/cmd'},\
                    {qmle_nethome:'Unit_Test_Cases/Case_0_1/macro_script.txt'},\
                    {qmle_nethome:'Unit_Test_Cases/Case_0_2/macro_script.txt'},\
-                    {imam_nethome1:'Case_16_up/macro_script.txt'}  ]
+                    {imam_nethome1:'Imam_Journal_Case_1/macro_script.txt'},\
+                    {imam_nethome1:'Imam_Journal_Case_2/macro_script.txt'}  ]
 
         for tc in tc_list:
             print("Case id:", tc_list.index(tc))
