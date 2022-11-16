c9d8ec30fcb5703e0ebe67c2a5a10cddbf14b48c
Yarui Peng <yrpeng@uark.edu>
2022-11-15 21:43:04 -0600

    update some folder struction in misc

--- general/settings/settings.py
+++ general/settings/settings.py
@@ -3,15 +3,15 @@ import os
 PSRoot=''
 
 #read-only folders
-MATERIAL_LIB_PATH = 'misc/PDK/Materials.csv'
-FASTHENRY_EXE = 'misc/FastHenry.exe'
-PARAPOWER_CODEBASE = 'misc/ParaPower'
-MANUAL = 'misc/PowerSynth_v2.0.pdf'
+MATERIAL_LIB_PATH = 'pkg/MDK/Materials.csv'
+FASTHENRY_EXE = 'pkg/bin/fasthenry'
+PARAPOWER_CODEBASE = 'pkg/ParaPower'
+MANUAL = 'pkg/man/PowerSynth_v2.0.pdf'
 
 PSTemp=''
 
 #temp folders
-FASTHENRY_FOLDER = './FastHenry'
-PARAPOWER_FOLDER = './ParaPower'
+FASTHENRY_FOLDER = 'FastHenry'
+PARAPOWER_FOLDER = 'ParaPower'
 
 PSWork=''
